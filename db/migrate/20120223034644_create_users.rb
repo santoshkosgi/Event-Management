eb7916f53e19f974d28309021a2183e04b3c2a96
santoshkosgi <santoshkosgi@gmail.com>
2012-02-23 13:52:15 +0530

    initial commit

---
+++ db/migrate/20120223034644_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :password
+      t.string :location
+      t.string :email
+      t.string :company
+      t.integer :role
+
+      t.timestamps
+    end
+  end
+end
